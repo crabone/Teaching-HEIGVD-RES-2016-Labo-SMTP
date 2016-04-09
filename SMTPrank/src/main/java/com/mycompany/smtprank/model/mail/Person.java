b6184815b87ba6c99b4512b0d877c46e159bed78
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-04-09 13:04:39 +0200

    Refactoring de la classe Person

--- SMTPrank/src/main/java/com/mycompany/smtprank/model/mail/Person.java
+++ SMTPrank/src/main/java/com/mycompany/smtprank/model/mail/Person.java
@@ -19,20 +19,20 @@ public class Person {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
     public String getAddress() {
         return address;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public void setAddress(String address) {
