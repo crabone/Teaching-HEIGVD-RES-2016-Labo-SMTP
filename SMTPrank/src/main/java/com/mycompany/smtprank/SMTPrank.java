32e32af6fe5067674dc4fc6b77fec061607433f1
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-04-09 16:02:22 +0200

    Ajout de la classe principale (non-implantée)

---
+++ SMTPrank/src/main/java/com/mycompany/smtprank/SMTPrank.java
@@ -0,0 +1,11 @@
+package com.mycompany.smtprank;
+
+/**
+ *
+ * @author crab_one
+ */
+public class SMTPrank {
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
