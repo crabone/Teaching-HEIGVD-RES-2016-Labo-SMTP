7d57b217d97b440f1f8fb429490034183449fad8
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-04-19 18:54:17 +0200

    [Add] Ajout d'un filtre permettant de convertir les terminaison de lignes en CRLF selon le protocole SMTP

---
+++ SMTPrank/src/main/java/com/mycompany/smtprank/filters/CRLFFilterWriter.java
@@ -0,0 +1,69 @@
+package com.mycompany.smtprank.filters;
+
+import com.mycompany.smtprank.Utils;
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ *
+ * @author crab_one
+ */
+public class CRLFFilterWriter extends FilterWriter {
+    
+    private int previousChar;
+
+    public CRLFFilterWriter(Writer out) {
+        super(out);
+    }
+    
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        String[] lines = new String[2];
+        String result = new String();
+        
+        lines[1] = str.substring(off, off + len);
+        
+        do {
+            lines = Utils.getNextLine(lines[1]);
+            
+            if (lines[0].equals("")) {
+                result += lines[1];
+                break;
+            }
+            else {
+                if (!lines[0].contains("\r\n")) {
+                    lines[0] = lines[0].substring(0, lines[0].length() - 1) + "\r\n";
+                }
+                result += lines[0];
+            }
+            
+        } while (!lines[0].equals(""));
+        
+        super.write(result, 0, result.length());
+    }
+    
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        write(new String(cbuf), off, len);
+    }
+    
+    @Override
+    public void write(int c) throws IOException {
+        if (c == '\r') {
+            previousChar = '\r';
+            return;
+        }
+        else if (c == '\n' && previousChar == '\r') {
+            previousChar = c;
+            write("\r\n", 0, 2);
+            return;
+        }
+        else if (c != '\n' && previousChar == '\r') {
+            previousChar = c;
+            write(new String(Character.toChars('\r')), 0, 1);
+        }
+
+        write(new String(Character.toChars(c)), 0, 1);
+    }
+}
---
+++ SMTPrank/src/test/java/com/mycompany/smtprank/filters/CRLFFilterWriterTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.smtprank.filters;
+
+import java.io.StringWriter;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author crab_one
+ */
+public class CRLFFilterWriterTest {
+    
+    @Test
+    public void itShouldWorkOnUnix() throws Exception {
+        String line = "This is line 1\nThis is line 2\nThis is line 3";
+        String expected = "This is line 1\r\nThis is line 2\r\nThis is line 3";
+        StringWriter stringWriter = new StringWriter();
+        CRLFFilterWriter writer = new CRLFFilterWriter(stringWriter);
+        writer.write(line);
+        Assert.assertEquals(expected, stringWriter.toString());
+    }
+    
+    @Test
+    public void itShouldWorkOnMacOS9() throws Exception {
+        String line = "This is line 1\rThis is line 2\rThis is line 3";
+        String expected = "This is line 1\r\nThis is line 2\r\nThis is line 3";    
+        StringWriter stringWriter = new StringWriter();
+        CRLFFilterWriter writer = new CRLFFilterWriter(stringWriter);
+        writer.write(line);
+        Assert.assertEquals(expected, stringWriter.toString());
+    }
+    
+    @Test
+    public void itShouldWorkOnWindows() throws Exception {
+        String line = "This is line 1\r\nThis is line 2\r\nThis is line 3";
+        String expected = "This is line 1\r\nThis is line 2\r\nThis is line 3";    
+        StringWriter stringWriter = new StringWriter();
+        CRLFFilterWriter writer = new CRLFFilterWriter(stringWriter);
+        writer.write(line);
+        Assert.assertEquals(expected, stringWriter.toString());
+    }
+    
+}
